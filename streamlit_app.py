f0f1fb6ce7a8941313477cc423f3274386b23fe1
sophiaray88 <sophiafelicity@gmail.com>
2024-08-24 13:14:13 -0500

    Update streamlit_app.py
    updated to include name on the order

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,22 +14,26 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list=st.multiselect (
     'Choose up to 5 ingredients:'
     ,my_dataframe
+    ,max_selections=5
 )
-
+# to change list to a string
 if ingredients_list:
     #st.write(ingredients_list)
     #st.text(ingredients_list)
     ingredients_string=''
     for fruit_chosen in ingredients_list: 
         ingredients_string += fruit_chosen+' '
-    st.write(ingredients_string)
-    my_insert_stmt="""insert into smoothies.public.orders(ingredients)
-              values('"""+ ingredients_string + """')"""
+    #st.write(ingredients_string)
+    my_insert_stmt="""insert into smoothies.public.orders(ingredients,name_on_order)
+              values('"""+ ingredients_string + """','"""+ name_on_order + """')""" 
+    #st.write(my_insert_stmt)
+    #st.stop()
     time_to_insert=st.button('Submit Order')
     if time_to_insert: 
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!', icon="✅")
+    
    
 
 
