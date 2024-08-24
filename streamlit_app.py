0178a6c100101a3cbda1e6379d1ab3ae135e991c
sophiaray88 <sophiafelicity@gmail.com>
2024-08-24 17:46:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,15 +18,14 @@ ingredients_list=st.multiselect (
     ,my_dataframe
     ,max_selections=5
 )
-# to change list to a string
+
 if ingredients_list:
-    #st.write(ingredients_list)
-    #st.text(ingredients_list)
     ingredients_string=''
+    
     for fruit_chosen in ingredients_list: 
         ingredients_string += fruit_chosen +' '
-        st.subheader(fruit_chosen+' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
     #st.write(ingredients_string)
